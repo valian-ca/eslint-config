a13d0cf9771c650c4a4d6d4bb898367e43cb7c78
Julien Marcil <julien@valian.ca>
2023-05-15 18:57:52 -0400

    chore: configure for beta release

--- .projenrc.js
+++ .projenrc.js
@@ -1,5 +1,4 @@
-const { javascript } = require('projen')
-const { exec } = require('projen/lib/util')
+const { javascript, release } = require('projen')
 const project = new javascript.NodeProject({
   packageName: '@valian/eslint-config',
   description: 'Valian ESLint Config',
@@ -24,6 +23,8 @@ const project = new javascript.NodeProject({
   prettierOptions: { settings: { semi: false, singleQuote: true, printWidth: 120 } },
   eslint: true,
   jest: false,
+
+  prerelease: 'beta',
 
   deps: [
     '@typescript-eslint/eslint-plugin',
@@ -54,6 +55,6 @@ const project = new javascript.NodeProject({
 project.addPeerDeps('eslint@>=8.40.0')
 project.addPeerDeps('typescript@>=4.9.5')
 
-const eslintTask = project.addTask('eslint', { description: 'Run eslint', exec: 'eslint .' })
+project.addTask('eslint', { description: 'Run eslint', exec: 'eslint .' })
 
 project.synth()
