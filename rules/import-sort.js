433cca3a4ffc4a9132661956331c8e6b5825dbf9
Julien Marcil <julien@valian.ca>
2024-05-20 13:13:34 -0400

    chore: npm run upgrade

--- rules/import-sort.js
+++ rules/import-sort.js
@@ -8,22 +8,22 @@ module.exports = {
       {
         groups: [
           // Side effect imports.
-          ['^\\u0000'],
+          [String.raw`^\u0000`],
           // Node.js builtins prefixed with `node:`.
           ['^node:'],
           ['^react'],
           // Packages.
           // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
-          ['^@?\\w'],
+          [String.raw`^@?\w`],
           // Absolute imports and other imports such as Vue-style `@/foo`.
           // Anything not matched in another group.
           ['^'],
           // Parent imports. Put `..` last.
-          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          [String.raw`^\.\.(?!/?$)`, String.raw`^\.\./?$`],
           // Other relative imports. Put same-folder imports and `.` last.
-          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          [String.raw`^\./(?=.*/)(?!/?$)`, String.raw`^\.(?!/?$)`, String.raw`^\./?$`],
           // Style imports.
-          ['^.+\\.?(css)$'],
+          [String.raw`^.+\.?(css)$`],
         ],
       },
     ],
